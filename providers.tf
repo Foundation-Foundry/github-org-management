16d3874a52f3be815057cf2279fac5b76cb227d9
Lubosz Kosnik <lubosz.kosnik@gmail.com>
2025-05-09 20:57:15 -0400

    Refactor providers and versions

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.5.7"
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = ">= 6.0.0"
     }
   }
 }
